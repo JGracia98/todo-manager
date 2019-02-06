8b4632f60e833a1ebfa00f87f5ce3572dbff2690
Justin Gracia <graciajustin13@gmail.com>
2019-02-06 14:45:27 -0500

    First commit

---
+++ item.py
@@ -0,0 +1,5 @@
+class Item(object):
+    # 1. A timestamp of when they were created
+    # 2. A boolean marking the item is completed or not
+    # 3. And the text of the actual to-do item
+    
---
+++ manager.py
@@ -0,0 +1,4 @@
+class Manager(object):
+    # 1. Print all of the to-do items in the list
+    # 2. Add a new item to the list
+    # 3. Mark an item as completed
