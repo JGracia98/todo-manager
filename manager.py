add42fdab093baee2b34d02629c1adf35aec8210
Justin Gracia <graciajustin13@gmail.com>
2019-02-07 16:13:57 -0500

    Tried to get my add function to work

--- manager.py
+++ manager.py
@@ -2,6 +2,7 @@ from sys import argv
 import item
 
 script, file_name = argv
+
 class Manager(object):
 
     def read(self):
@@ -13,9 +14,8 @@ class Manager(object):
     def add_to_list(self, item):
 
         target = open(file_name, 'a')
-
-        target.write()
-
+        item = input('What do you want do add')
+        print(target.write(item))
         target.close()
 
 text = Manager()
