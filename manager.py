58e9c6d8a9e1e5e3f618b5a192d03fa42b3c6e44
Justin Gracia <graciajustin13@gmail.com>
2019-02-10 23:10:28 -0500

    Got the todo to work without using the sys import

--- manager.py
+++ manager.py
@@ -1,19 +1,19 @@
-from sys import argv
+# from sys import argv
 import item
 
-script, file_name = argv
+# script, file_name = argv
 
 class Manager(object):
 
     print("Welcome to your to-do manager!")
     def read(self):
-        target = open(file_name, 'r')
+        target = open('todos.txt', 'r')
         print(target.read())
         target.close()
 
     def add_to_list(self):
 
-        target = open(file_name, 'a')
+        target = open('todos.txt', 'a')
         add_item = target.write(input('What do you want do add? '))
 #        print(target.write(item))
         target.close()
